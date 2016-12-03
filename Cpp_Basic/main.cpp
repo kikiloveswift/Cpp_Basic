af777606620cf5396e9a8a72dd5d3032b9365dbb
Kingkonglee <konglee873@gmail.com>
2016-12-03 14:10:23 +0800

    Initial Commit

---
+++ Cpp_Basic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp_Basic
+//
+//  Created by kong on 16/12/3.
+//  Copyright © 2016年 kong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
